a7bc81384837d0e1f7d22ea2c8041546df254b73
colenocks <enockscoleman@gmail.com>
2019-04-11 08:21:37 +0100

    ....

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 //const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1337;
 const server = app.listen(port);
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
